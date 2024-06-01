43f028bc081b3ceede375e8ba802cf0500d18b00
Elysa Cassia <148411032+cassia1714@users.noreply.github.com>
2024-06-01 05:47:26 -0300

    enviando

--- ProjetoComLysa/index.py
+++ ProjetoComLysa/index.py
@@ -1,1 +1,36 @@
-print ("Hello Word")
+import tkinter as tk
+from tkinter import messagebox
+
+def fazer_login():
+
+    usuario = entry_usuario.get()
+    senha = entry_senha.get()
+
+   
+    if usuario == "admin" and senha == "admin123":
+        messagebox.showinfo("Login", "Login bem sucedido!")
+       
+    else:
+        messagebox.showerror("Login", "Nome de usuário ou senha incorretos")
+
+# Criar janela
+janela = tk.Tk()
+janela.title("Tela de Login")
+
+# Criar rótulos e campos de entrada
+label_usuario = tk.Label(janela, text="Nome de Usuário:")
+label_usuario.pack()
+entry_usuario = tk.Entry(janela)
+entry_usuario.pack()
+
+label_senha = tk.Label(janela, text="Senha:")
+label_senha.pack()
+entry_senha = tk.Entry(janela, show="*")  # O texto da senha será ocultado
+entry_senha.pack()
+
+# Botão de login
+botao_login = tk.Button(janela, text="Login", command=fazer_login)
+botao_login.pack()
+
+# Executar a janela principal
+janela.mainloop() 
