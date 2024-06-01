730760965ef2ab2c5ee1ac6c38c61ff91a8e12db
Hugo-Henrique7 <hugo.ccbh7@gmail.com>
2024-06-01 04:35:41 -0300

    Ajeitando os arquivos

--- ProjetoComLysa/index.py
+++ ProjetoComLysa/index.py
@@ -1,1 +1,13 @@
-print ("Hello Word")
+import tkinter as tk
+
+# Criar uma janela
+janela = tk.Tk()
+
+# Criar um rótulo
+rotulo = tk.Label(janela, text="Olá, mundo!")
+
+# Exibir o rótulo na janela
+rotulo.pack()
+
+# Executar a janela principal
+janela.mainloop()
